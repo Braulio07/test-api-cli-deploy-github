8f3cb70d17ec3da87cbcbc1c4034a0cf87b4f825
Braulio Ovalles <braulioovalles7d@outlook.com>
2026-09-17 15:41:23 +0200

    Feat: adding comment

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hola, soy el API de servicio');
 });
 
+//Adding comment
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
